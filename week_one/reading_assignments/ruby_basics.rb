f91f8ee9f7053220a71abff3b5e170428391c242
John McGarvey <jmcgarve@bellsouth.net>
2018-09-20 23:14:33 +0000

    Create initial git repository

---
+++ week_one/reading_assignments/ruby_basics.rb
@@ -0,0 +1,30 @@
+# answer each of the following to the best of your ability
+# there is always more than one way to code a response 😄
+# please use your knowledge of variable assignments in each response
+# when you are done, share your code with your instructor, push it to Github, or submit a pull request
+
+
+# output an integer
+
+var = 7
+puts "Here is an integer: #{var}"
+
+# output the number of seconds in a week
+
+var = 7 * 24 * 60 * 60
+puts "There are #{var} seconds in a week."
+
+# output a float
+
+var = 3.14159 
+puts "#{var} is a float."
+
+# output a string
+
+var = "Any string will do."
+puts var
+
+# output concatination of strings
+
+var  = "this begins it, " + "and this ends it."
+puts var
